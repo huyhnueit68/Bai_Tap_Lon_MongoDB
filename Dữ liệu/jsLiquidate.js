5fefcad2dc7725c7f5aaecf2caa706bf9191ce85
huyhnueit68 <huypq_sv@hnue.edu.vn>
2020-12-16 00:49:17 +0700

    update data base for project

--- Dữ liệu/jsLiquidate.js
+++ Dữ liệu/jsLiquidate.js
@@ -3,7 +3,7 @@ var Liqui = [
         Id_Liqui: 1,
         Name_Liqui: "Thanh lý thiết bị hỏng",
         Date_Liqui: new Date("2020-01-01"),
-        Id_Device: 1,
+        Id_Device: 16,
         Name_Device: "HP Omen 15inch",
         Room: "K505",
         Id_Type: 1
@@ -12,7 +12,7 @@ var Liqui = [
         Id_Liqui: 2,
         Name_Liqui: "Thanh ổ cứng",
         Date_Liqui: new Date("2019-12-14"),
-        Id_Device: 2,
+        Id_Device: 17,
         Name_Device: "Hard santa",
         Room: "K507",
         Id_Type: 2
@@ -21,7 +21,7 @@ var Liqui = [
         Id_Liqui: 3,
         Name_Liqui: "Thanh ổ cứng 2",
         Date_Liqui: new Date("2019-12-14"),
-        Id_Device: 3,
+        Id_Device: 18,
         Name_Device: "Hard santa 300",
         Room: "K507",
         Id_Type: 2
